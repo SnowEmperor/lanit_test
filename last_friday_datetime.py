b984f24e802875c43e4473816386982eef739e92
SnowEmperor <99719069+SnowEmperor@users.noreply.github.com>
2023-06-04 23:52:01 +0300

    Add files via upload

---
+++ last_friday_datetime.py
@@ -0,0 +1,10 @@
+import datetime
+from dateutil.relativedelta import FR, relativedelta
+
+
+def last_friday(d):
+    d = datetime.datetime.strptime("01/" + d, "%d/%m/%Y")
+    res = d + relativedelta(day=31, weekday=FR(-1))
+    return res.strftime("%d.%m.%Y")
+
+
