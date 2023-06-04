c54042f20142db3a07fffe30571142552b733ef1
SnowEmperor <99719069+SnowEmperor@users.noreply.github.com>
2023-06-05 00:44:44 +0300

    Update last_friday_datetime.py

--- last_friday_datetime.py
+++ last_friday_datetime.py
@@ -1,14 +1,10 @@
-import calendar
+import datetime
+from dateutil.relativedelta import FR, relativedelta
 
 
-def last_friday(d):
-    m, y = d.split('/')
-    day = calendar.monthcalendar(int(y), int(m))[-1][4]
-    if not day:
-        day = calendar.monthcalendar(int(y), int(m))[-2][4]
-    day = str(day)
-    if len(day) == 1:
-        day = '0' + day
-    return f'{day}.{m}.{y}'
+def get_last_friday(d):
+    d = datetime.datetime.strptime("01/" + d, "%d/%m/%Y")
+    res = d + relativedelta(day=31, weekday=FR(-1))
+    return res.strftime("%d.%m.%Y")
 
 
